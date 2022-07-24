e43f9f3c9aa99259b8ec53970633e43e666c0fba
Truston Ailende <trustonailende@gmail.com>
2022-07-24 10:39:22 +0100

    Creation of User interface using free texture packer

--- js/game.js
+++ js/game.js
@@ -51,20 +51,7 @@ class bootGame extends Phaser.Scene {
         this.load.image("bg_plain", "assets/images/bg_plain.png");
         this.load.image("pos_target_num", "assets/images/pos_target_num.png");
         this.load.image("pos_buttons", "assets/images/pos_buttons.png");
-        this.load.image("0-display", "assets/images/0-display.png");
-        this.load.image("clear-red", "assets/images/clear-red.png");
-        this.load.image("1-over", "assets/images/1-over.png");
-        this.load.image("2-over", "assets/images/2-over.png");
-        this.load.image("3-over", "assets/images/3-over.png");
-        this.load.image("4-over", "assets/images/4-over.png");
-        this.load.image("5-over", "assets/images/5-over.png");
-        this.load.image("6-over", "assets/images/6-over.png");
-        this.load.image("7-over", "assets/images/7-over.png");
-        this.load.image("8-over", "assets/images/8-over.png");
-        this.load.image("9-over", "assets/images/9-over.png");
-        this.load.image("start_button", "assets/images/start_button.png");
-        this.load.image("retry_b", "assets/images/retry_b.png");
-        this.load.image("info_b", "assets/images/info_b.png");
+        this.load.atlas('atlas', 'assets/images/texture.png', 'assets/images/texture.json');
 
         this.load.on('progress', function(value) {
             progressBar.clear();
@@ -102,24 +89,24 @@ class gameScreen extends Phaser.Scene {
         var bg_plain = this.add.image(270, 480, "bg_plain");
         var pos_target_num = this.add.image(270, 480, "pos_target_num");
         var pos_buttons = this.add.image(270, 480, "pos_buttons");
-        var target = this.add.image(270, 480, "0-display");
-        var clear = this.add.image(270, 700, "clear-red");
-        var one = this.add.image(140, 660, "1-over");
-        var two = this.add.image(60, 550, "2-over");
-        var three = this.add.image(60, 410, "3-over");
-        var four = this.add.image(140, 300, "4-over");
-        var five = this.add.image(270, 260, "5-over");
-        var six = this.add.image(400, 300, "6-over");
-        var seven = this.add.image(480, 410, "7-over");
-        var eight = this.add.image(480, 550, "8-over");
-        var nine = this.add.image(400, 660, "9-over");
-        var start = this.add.image(270, 870, "start_button");
-        start.setScale(2);
-        var retry = this.add.image(100, 860, "retry_b");
-        var info = this.add.image(450, 860, "info_b");
         var timeText = this.add.text(20, 50, 'Time: 0', { fontSize: '40px', fill: '#fff', fontFamily: 'roboto-slab' });
 		var totalText = this.add.text(170, 50, 'Total: 0', { fontSize: '40px', fill: '#fff', fontFamily: 'roboto-slab' });
 		var scoreText = this.add.text(340, 50, 'Score: 0', { fontSize: '40px', fill: '#fff', fontFamily: 'roboto-slab' });
+        var one = this.add.sprite(140, 660, 'atlas', '1-over.png');
+        var two = this.add.sprite(60, 550, 'atlas', '2-over.png');
+        var three = this.add.sprite(60, 410, 'atlas', '3-over.png');
+        var four = this.add.sprite(140, 300, 'atlas', '4-over.png');
+        var five = this.add.sprite(270, 260, 'atlas', '5-over.png');
+        var six = this.add.sprite(400, 300, 'atlas', '6-over.png');
+        var seven = this.add.sprite(480, 410, 'atlas', '7-over.png');
+        var eight = this.add.sprite(480, 550, 'atlas', '8-over.png');
+        var nine = this.add.sprite(400, 660, 'atlas', '9-over.png');
+        var clear = this.add.sprite(270, 700, 'atlas', 'clear-red.png');
+        var target = this.add.sprite(270, 480, 'atlas', '0-display.png');
+        var start = this.add.sprite(270, 870, 'atlas', 'start_button.png');
+        start.setScale(2);
+        var retry = this.add.sprite(100, 860, 'atlas', 'retry_b.png');
+        var info = this.add.sprite(450, 860, 'atlas', 'info_b.png');
     }
 }
 
